1e474114ab46f43d0ab288f2673fd618d3adfe1e
knottedTail <philip94@snu.ac.kr>
2023-02-13 16:43:58 +0900

    Gatsby tutorial Part 2

--- gatsby_tutorial/RECORDING.md
+++ gatsby_tutorial/RECORDING.md
@@ -1,1 +1,8 @@
-Following the official Gatsby tutorial: https://www.gatsbyjs.com/docs/tutorial/part-0/
+Gatsby tutorials
+* [Part 0, Setting](https://www.gatsbyjs.com/docs/tutorial/part-0/)
+* [Part 1, Create pages](https://www.gatsbyjs.com/docs/tutorial/part-1/)
+* [Part 2, Custom React compo, CSS "module"](https://www.gatsbyjs.com/docs/tutorial/part-2/)
+
+**Appendix**
+
+[About adding SEO components.](https://www.gatsbyjs.com/docs/how-to/adding-common-features/adding-seo-component/)
---
+++ gatsby_tutorial/src/pages/about.js
@@ -0,0 +1,29 @@
+// Step 1: Import React
+import * as React from 'react'
+// import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Knotted World!">
+        <p>We are falling into knotted things.</p>
+    </Layout>
+    // <main>
+    //   <h1>About Knotted World!</h1>
+    //   <Link to="/">Back to home?</Link>
+    //   <p>We are falling into knotted things.</p>
+    // </main>
+  )
+}
+
+// // You'll learn about this in the next task, just copy it for now
+export const Head = () => (
+    <>
+        <title>About Page Damnnnnn</title>
+        <meta name="description" content="Delta yourself"/>
+    </>
+)
+
+// Step 3: Export your component
+export default AboutPage
