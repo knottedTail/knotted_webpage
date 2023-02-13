7f266047fbd0b2267ba9eaf0a36e2ffd4c700349
knottedTail <philip94@snu.ac.kr>
2023-02-13 23:04:43 +0900

    GATSBY TUTORIAL PART 7

--- gatsby_tutorial/RECORDING.md
+++ gatsby_tutorial/RECORDING.md
@@ -6,11 +6,13 @@ Gatsby tutorials
 * [Part 4, Query and GraphQL, page query!](https://www.gatsbyjs.com/docs/tutorial/part-4/)
     * NEED a source plugin for each specific source type (to pull data to the site)
 * [Part 5, Dealing with .mdx files(transforming from File nodes to MDX nodes), graphQL frontmatter, formatString](https://www.gatsbyjs.com/docs/tutorial/part-5/)
-* [Part 6, Programmatically generating pages, slugify, query variables, ](https://www.gatsbyjs.com/docs/tutorial/part-6/)
-
+* [Part 6, Programmatically generating pages, File System Route API!, slugify, query variables](https://www.gatsbyjs.com/docs/tutorial/part-6/)
+* [Part 7, ](https://www.gatsbyjs.com/docs/tutorial/part-7/)
 
 **Appendix**
 
 [About adding SEO components.](https://www.gatsbyjs.com/docs/how-to/adding-common-features/adding-seo-component/)
 
 [How to GraphQL.](https://www.howtographql.com/)
+
+[Preoptimize Images.](https://www.gatsbyjs.com/docs/preoptimizing-images/)
--- gatsby_tutorial/src/pages/blog/{mdx.frontmatter__slug}.js
+++ gatsby_tutorial/src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,15 +1,27 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = ({ data, children }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
 // const BlogPost = ( props ) => {
     // console.log(props)
     return (
         <Layout pageTitle={ data.mdx.frontmatter.title }>
             {/* <p> My Blog Post CONTENTS WILL GO HEEEERE (eventually) </p> */}
-            <p> { data.mdx.frontmatter.date } </p>
+            <p>Posted: { data.mdx.frontmatter.date } </p>
+            <GatsbyImage
+                image= { image }
+                alt= { data.mdx.frontmatter.hero_image_alt }
+            />
+            <p> 
+                Photo Credit:{" "}
+                <a href={ data.mdx.frontmatter.hero_image_credit_link }>
+                    { data.mdx.frontmatter.hero_image_credit_text }
+                </a>
+            </p>
             {children}
         </Layout>
     )
@@ -21,6 +33,14 @@ export const query = graphql`
             frontmatter {
                 title
                 date(formatString: "MMMM D, YYYY")
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image_alt
+                hero_image {
+                  childImageSharp {
+                    gatsbyImageData
+                  }
+                }
             }
         }
     }
