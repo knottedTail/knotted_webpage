baf632bd0b6ee9b3eb653985f22a69d2c1b9f34d
knottedTail <philip94@snu.ac.kr>
2023-02-13 16:59:32 +0900

    Gatsby tutorial Part 3

--- gatsby_tutorial/RECORDING.md
+++ gatsby_tutorial/RECORDING.md
@@ -1,7 +1,8 @@
 Gatsby tutorials
 * [Part 0, Setting](https://www.gatsbyjs.com/docs/tutorial/part-0/)
 * [Part 1, Create pages](https://www.gatsbyjs.com/docs/tutorial/part-1/)
-* [Part 2, Custom React compo, CSS "module"](https://www.gatsbyjs.com/docs/tutorial/part-2/)
+* [Part 2, Pre-built(Link) and Custom(Layout) React compo, CSS "module"](https://www.gatsbyjs.com/docs/tutorial/part-2/)
+* [Part 3, Plugin(Handling photo!)](https://www.gatsbyjs.com/docs/tutorial/part-3/)
 
 **Appendix**
 
--- gatsby_tutorial/src/pages/index.js
+++ gatsby_tutorial/src/pages/index.js
@@ -2,12 +2,19 @@
 import * as React from 'react'
 import Layout from "../components/layout"
 import MyComponent from '../components/my-component'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="HOME page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        title="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        // src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        src="../images/gatsby-icon.png"
+      />
       <MyComponent> Hitit! </MyComponent> 
     </Layout>
     // <main>
